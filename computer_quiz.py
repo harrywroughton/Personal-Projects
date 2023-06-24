6101aa45dd53eff7364b0048785eb2a8e4a47885
Harry Wroughton <126293012+harrywroughton@users.noreply.github.com>
2023-06-24 09:45:54 +0100

    Add files via upload

--- computer_quiz.py
+++ computer_quiz.py
@@ -2,12 +2,15 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? ")
 
+# user starts game and exits if statement
 if playing.lower() != "yes":
     quit()
 
+# score determined from start
 print("OK, let's play!")
 score = 0
 
+# add point to score for all questions correct, otherwise score remains the same and moves to next Q
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
     print("Correct!")
@@ -36,5 +39,6 @@ if answer.lower() == "graphics processing unit":
 else:
     print("Sorry, your answer is incorrect!")
 
+# convert score to string to give output of score and percentage
 print("You got " + str(score) + " question correct!")
 print("You got " + str((score / 4) * 100) + "%.")
